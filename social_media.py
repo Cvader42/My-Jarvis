7b47f1bae5276bc11f78290f355f3cc3736ee136
Jeremy Dafler <134081715+Cvader42@users.noreply.github.com>
2023-08-19 07:00:34 -0700

    Create social_media.py

---
+++ social_media.py
@@ -0,0 +1,8 @@
+# social_media.py
+def post_to_twitter(message):
+    # Implement code to post to Twitter
+    return f"Posted to Twitter: {message}"
+
+def post_to_facebook(message):
+    # Implement code to post to Facebook
+    return f"Posted to Facebook: {message}"
