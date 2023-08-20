c26e92f2e9326fdb526ba49b5302773c3614f76a
Jeremy Dafler <134081715+Cvader42@users.noreply.github.com>
2023-08-20 03:03:35 -0700

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -1,1 +1,30 @@
+import calendar
+
+class AdvancedCalendar:
+    def __init__(self):
+        self.cal = calendar.Calendar(firstweekday=calendar.SUNDAY)
+        self.month_names = calendar.month_name[1:]
+    
+    def get_month(self, year, month):
+        return self.cal.monthdayscalendar(year, month)
+    
+    def get_month_name(self, month):
+        return self.month_names[month - 1]
+
+# Example usage
+if __name__ == "__main__":
+    advanced_calendar = AdvancedCalendar()
+    year = 2023
+    month = 8
+    month_name = advanced_calendar.get_month_name(month)
+    month_days = advanced_calendar.get_month(year, month)
+    
+    print(f"Calendar for {month_name} {year}:")
+    for week in month_days:
+        for day in week:
+            if day == 0:
+                print("   ", end=" ")
+            else:
+                print(f"{day:2d}", end=" ")
+        print()
 
