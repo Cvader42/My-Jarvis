2c645cb4815bfbcc7df0cee5d9aa5087f6a948f4
Jeremy Dafler <134081715+Cvader42@users.noreply.github.com>
2023-08-20 04:43:20 -0700

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,1 +1,30 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+class SentimentAnalyzer:
+    def __init__(self):
+        nltk.download('vader_lexicon')  # Download the required lexicon
+        self.sia = SentimentIntensityAnalyzer()
+
+    def analyze_sentiment(self, text):
+        sentiment_score = self.sia.polarity_scores(text)
+        sentiment = self.get_sentiment_label(sentiment_score)
+        return sentiment, sentiment_score
+
+    def get_sentiment_label(self, sentiment_score):
+        compound_score = sentiment_score['compound']
+        if compound_score >= 0.05:
+            return "Positive"
+        elif compound_score <= -0.05:
+            return "Negative"
+        else:
+            return "Neutral"
+
+if __name__ == "__main__":
+    text = input("Enter the text to analyze sentiment: ")
+    analyzer = SentimentAnalyzer()
+    sentiment, sentiment_score = analyzer.analyze_sentiment(text)
+    
+    print("Sentiment:", sentiment)
+    print("Sentiment Scores:", sentiment_score)
 
