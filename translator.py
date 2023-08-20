c9e5add4b8595933ca3d6b11592c2c3e34970cd0
Jeremy Dafler <134081715+Cvader42@users.noreply.github.com>
2023-08-20 04:20:04 -0700

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,1 +1,23 @@
+from translate import Translator
+
+class AdvancedTranslator:
+    def __init__(self):
+        self.translator = Translator(to_lang="en")
+    
+    def translate(self, text, target_language):
+        try:
+            translation = self.translator.translate(text, to_lang=target_language)
+            return translation
+        except Exception as e:
+            return f"Translation error: {str(e)}"
+
+# Example usage
+if __name__ == "__main__":
+    advanced_translator = AdvancedTranslator()
+    text_to_translate = "Hello, how are you?"
+    target_language = "es"
+    translated_text = advanced_translator.translate(text_to_translate, target_language)
+    
+    print(f"Original Text: {text_to_translate}")
+    print(f"Translated Text ({target_language}): {translated_text}")
 
