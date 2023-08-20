9e5509a117cc7a172c4f4e9ffa3f3710ad23f96f
Jeremy Dafler <134081715+Cvader42@users.noreply.github.com>
2023-08-20 04:56:19 -0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,1 +1,40 @@
+# api_keys.py
+
+# Openai Api key
 OPENAI_API_KEY = "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"
+
+# Google Custom Search API key
+GOOGLE_CUSTOM_SEARCH_KEY = "your_google_custom_search_key_here"
+
+# OpenWeatherMap API key
+OPENWEATHERMAP_KEY = "your_openweathermap_key_here"
+
+# News API key
+NEWS_API_KEY = "your_news_api_key_here"
+
+# Google Custom search API key
+
+
+# OpenWeatherMap API key
+
+
+# News API Key
+
+
+# Language Translation API Key
+
+
+# Text-to-Speech API Key
+
+
+# Speech Recognition API Key
+
+
+# Social Media API Keys
+Twitter_API:
+
+Facebook_API:
+
+Instagram_API:
+
+Linkedin_API:
