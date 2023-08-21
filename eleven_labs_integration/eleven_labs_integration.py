1728cb5f2e05c2e0bec087e01f4a94781e592979
Jeremy Dafler <134081715+Cvader42@users.noreply.github.com>
2023-08-21 04:45:11 -0700

    Update eleven_labs_integration.py

--- eleven_labs_integration/eleven_labs_integration.py
+++ eleven_labs_integration/eleven_labs_integration.py
@@ -1,1 +1,23 @@
+# eleven_labs_integration.py
+from config import ELEVEN_LABS_API_KEY
+import requests
+
+def generate_speech(text):
+    url = "https://api.eleven-labs.com/tts"
+    headers = {
+        "Authorization": f"Bearer {ELEVEN_LABS_API_KEY}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "text": text
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        with open("output.mp3", "wb") as f:
+            f.write(response.content)
+        print("Speech generated and saved as 'output.mp3'")
+    else:
+        print("Error:", response.text)
 
