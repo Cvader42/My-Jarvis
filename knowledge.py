d308015239068865be15cb5cec4f503a4757124d
Jeremy Dafler <134081715+Cvader42@users.noreply.github.com>
2023-08-22 10:11:26 -0700

    Update knowledge.py

--- knowledge.py
+++ knowledge.py
@@ -1,1 +1,69 @@
+import json
+import nltk
+import spacy
+import tensorflow as tf
+from tensorflow import keras
 
+# This is the knowledge base for the Jarvis voice assistant.
+# It is a dictionary where the keys are the questions and the values are the answers.
+knowledge_base = {}
+
+# Load the biggest knowledge base available.
+with open("biggest_knowledge_base.json", "r") as f:
+    knowledge_base = json.load(f)
+
+# Use the most powerful natural language processing library available.
+nlp = spacy.load("distilbert-base-uncased")
+
+# Add machine learning to improve the accuracy of the answers.
+model = keras.models.load_model("trained_model.h5")
+
+def get_answer(question):
+    # This function returns the answer to the given question.
+    if question in knowledge_base:
+        return knowledge_base[question]
+    else:
+        # Use natural language processing to try to answer the question.
+        tokens = nlp(question)
+        answer = model.predict(tokens)
+
+        # If we can't find an answer, we will return a message saying that we don't know.
+        if answer == []:
+            return "I don't know the answer to that question."
+        else:
+            return answer[0]
+
+def search_answers(question):
+    # This function returns a list of answers to the given question.
+    answer_list = []
+    for key, value in knowledge_base.items():
+        if key.lower().startswith(question.lower()):
+            answer_list.append(value)
+    return answer_list
+
+def main():
+    question = input("What is your question? ")
+    answer = get_answer(question)
+    print(answer)
+
+def create_custom_knowledge_base():
+    # This function allows users to create custom knowledge bases.
+    new_knowledge_base = {}
+
+    # Get the user's input.
+    question = input("Enter a question: ")
+    answer = input("Enter the answer: ")
+
+    # Add the question and answer to the knowledge base.
+    new_knowledge_base[question] = answer
+
+    # Save the knowledge base to a file.
+    with open("custom_knowledge_base.json", "w") as f:
+        json.dump(new_knowledge_base, f)
+
+if __name__ == "__main__":
+    # Run the main function.
+    main()
+
+    # Give the user the option to create a custom knowledge base.
+    create_custom_knowledge_base()
