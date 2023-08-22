f9c041414995e3f63d2bdb790f5929c800fb5889
Jeremy Dafler <134081715+Cvader42@users.noreply.github.com>
2023-08-22 14:12:41 -0700

    Update eleven_labs_integration.py

--- eleven_labs_integration/eleven_labs_integration.py
+++ eleven_labs_integration/eleven_labs_integration.py
@@ -1,8 +1,14 @@
-# eleven_labs_integration.py
+"""
+Module to integrate with the Eleven Labs Text-to-Speech API.
+"""
+
 from config import ELEVEN_LABS_API_KEY
 import requests
 
 def generate_speech(text):
+    """
+    Generate speech from the given text using the Eleven Labs API.
+    """
     url = "https://api.eleven-labs.com/tts"
     headers = {
         "Authorization": f"Bearer {ELEVEN_LABS_API_KEY}",
@@ -15,9 +21,13 @@ def generate_speech(text):
     response = requests.post(url, headers=headers, json=data)
 
     if response.status_code == 200:
-        with open("output.mp3", "wb") as f:
-            f.write(response.content)
+        with open("output.mp3", "wb") as output_file:
+            output_file.write(response.content)
         print("Speech generated and saved as 'output.mp3'")
     else:
         print("Error:", response.text)
 
+# Example usage
+if __name__ == "__main__":
+    input_text = "Hello, this is a test speech."
+    generate_speech(input_text)
