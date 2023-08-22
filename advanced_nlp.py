1a348af4c5ffa3d9111f3e9a84441dfc3fdb93b2
Jeremy Dafler <134081715+Cvader42@users.noreply.github.com>
2023-08-22 14:08:57 -0700

    Update advanced_nlp.py

--- advanced_nlp.py
+++ advanced_nlp.py
@@ -1,18 +1,32 @@
+"""
+Module to implement the AdvancedNLP class for advanced natural language processing tasks.
+"""
+
 import transformers
 import spacy
 
 class AdvancedNLP:
+    """
+    Class to perform advanced natural language processing tasks using transformers and spaCy.
+    """
+
     def __init__(self):
         self.sentiment_analyzer = transformers.pipeline("sentiment-analysis")
         self.nlp = spacy.load("en_core_web_sm")
     
     def analyze_sentiment(self, text):
+        """
+        Analyze sentiment of the given text.
+        """
         sentiment = self.sentiment_analyzer(text)[0]
         sentiment_label = "Positive" if sentiment['label'] == 'LABEL_1' else "Negative"
         sentiment_score = sentiment['score']
         return f"Sentiment: {sentiment_label} (Score: {sentiment_score:.4f})"
     
     def extract_named_entities(self, text):
+        """
+        Extract named entities from the given text.
+        """
         doc = self.nlp(text)
         entities = [(ent.text, ent.label_) for ent in doc.ents]
         return entities
@@ -20,13 +34,12 @@ class AdvancedNLP:
 # Example usage
 if __name__ == "__main__":
     nlp = AdvancedNLP()
-    text = "I'm extremely excited about the upcoming event with amazing speakers."
+    input_text = "I'm extremely excited about the upcoming event with amazing speakers."
     
-    sentiment_analysis = nlp.analyze_sentiment(text)
-    named_entities = nlp.extract_named_entities(text)
+    sentiment_analysis = nlp.analyze_sentiment(input_text)
+    named_entities = nlp.extract_named_entities(input_text)
     
     print(sentiment_analysis)
     print("Named Entities:")
     for entity, label in named_entities:
         print(f"{entity} - {label}")
-
