933ea44bd1eca613010f40f88adb9b204619b8c0
Jeremy Dafler <134081715+Cvader42@users.noreply.github.com>
2023-08-22 22:30:56 -0700

    Update health_fitness_tracking.py

--- health_fitness_tracking.py
+++ health_fitness_tracking.py
@@ -1,13 +1,37 @@
 class ExerciseTracker:
+    """
+    A class to track exercise routines, suggest workouts, and provide health tips.
+    """
+
     def __init__(self):
+        """
+        Initialize the exercise tracker.
+        """
         self.routines = []
 
     def track_exercise_routine(self, exercise_type, duration):
-        # This method tracks an exercise routine.
-        self.routines.append({"exercise_type": exercise_type, "duration": duration})
+        """
+        Track an exercise routine.
+
+        Args:
+            exercise_type (str): The type of exercise.
+            duration (int): The duration of the exercise in minutes.
+        """
+        self.routines.append({
+            "exercise_type": exercise_type,
+            "duration": duration
+        })
 
     def suggest_workouts(self, workout_level):
-        # This method suggests workouts for a given workout level.
+        """
+        Suggest workouts for a given workout level.
+
+        Args:
+            workout_level (str): The workout level.
+
+        Returns:
+            list: A list of suggested workouts.
+        """
         if workout_level == "beginner":
             return ["30-minute brisk walk", "10-minute bodyweight exercises"]
         elif workout_level == "intermediate":
@@ -18,7 +42,15 @@ class ExerciseTracker:
             return []
 
     def provide_health_tips(self, health_topic):
-        # This method provides health tips for a given health topic.
+        """
+        Provide health tips for a given health topic.
+
+        Args:
+            health_topic (str): The health topic.
+
+        Returns:
+            str: A health tip.
+        """
         if health_topic == "nutrition":
             return "Stay hydrated and include a variety of fruits and vegetables in your diet."
         elif health_topic == "sleep":
@@ -27,6 +59,3 @@ class ExerciseTracker:
             return "Practice relaxation techniques such as deep breathing and meditation."
         else:
             return "I'm sorry, I don't have tips for that topic."
-
-# Create an instance of ExerciseTracker
-tracker = ExerciseTracker()
